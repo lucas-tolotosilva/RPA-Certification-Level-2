8d8fbda584840f568dc04f0dbf11fb52a8d5677d
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-10-05 12:00:28 -0300

    🎈Open the Web Page and download de excel file

--- tasks.py
+++ tasks.py
@@ -1,6 +1,26 @@
 from robocorp.tasks import task
+from robocorp import browser
+
+from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def order_robots_from_RobotSpareBin():
+    """
+    Orders robots from RobotSpareBin Industries Inc.
+    Saves the order HTML receipt as a PDF file.
+    Saves the screenshot of the ordered robot.
+    Embeds the screenshot of the robot to the PDF receipt.
+    Creates ZIP archive of the receipts and the images.
+    """
+    open_browser()
+    download_excel()
+
+def open_browser():
+    """Navigates to the given URL"""
+    browser.goto("https://robotsparebinindustries.com/#/")    
+
+def download_excel():
+    """Downloads excel file from the given URL"""
+    http = HTTP()
+    http.download(url="https://robotsparebinindustries.com/orders.csv", overwrite=True)
